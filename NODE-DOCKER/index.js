e3ac6e56c50a90b610a1f8be3ea01f180213bbb1
Ramjonchhen <ramjonchen@gmail.com>
2021-06-15 05:20:04 +0545

    Mongo container and mongoose

--- NODE-DOCKER/index.js
+++ NODE-DOCKER/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+// connecting to mongodb through mongoose
+mongoose
+  .connect("mongodb://ram:test@mongo/?authSource=admin")
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hi There ram here checking in development level 2</h2>");
+  res.send("<h2>Hi There ram here checking in development level 222</h2>");
 });
 
 app.listen(port, () => {
